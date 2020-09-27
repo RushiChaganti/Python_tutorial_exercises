f6a0e1e0021a727be967bd7892ccc2105e15331b
Rushi <rushi2004rushi@gmail.com>
2020-09-27 17:33:10 +0530

    submitting class 4 classwork

---
+++ todayclass27-9-2020/HW6.py
@@ -0,0 +1,12 @@
+favorite_fruits = ['apple', 'orange', 'pineapple']
+
+if 'bananas' in favorite_fruits:
+    print("You really like bananas!")
+if 'apples' in favorite_fruits:
+    print("You really like apples!")
+if 'guava' in favorite_fruits:
+    print("You really like guava!")
+if 'kiwis' in favorite_fruits:
+    print("You really like kiwis!")
+if 'custard apple' in favorite_fruits:
+    print("You really like custard apple!")
